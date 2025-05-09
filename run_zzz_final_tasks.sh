76a0491b14f4843a0ead5bf0174a9130df746989
Markus Binsteiner <markus@frkl.io>
2025-05-09 10:20:10 +0200

    chore: debug output

--- run_zzz_final_tasks.sh
+++ run_zzz_final_tasks.sh
@@ -4,6 +4,7 @@ echo "Running final tasks..."
 which code
 if command -v code 2>&1 >/dev/null
 then
+    echo "VSCode is installed, importing extensions..."
     ~/.local/scripts/manage-vscode-extensions.sh import
 fi
 
