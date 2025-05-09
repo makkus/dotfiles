b2916bb08170b336b93650a23336d2d5ca57bf43
Markus Binsteiner <markus@frkl.io>
2025-05-09 15:58:06 +0200

    fix: paths

--- dot_config/private_fish/conf.d/path.fish
+++ dot_config/private_fish/conf.d/path.fish
@@ -4,11 +4,17 @@ if test -n "$CONTAINER_ID" -a -n "$DISTROBOX_HOST_HOME"
     set -l actual_home $HOME
     
     # Add container-specific paths with higher precedence (prepend)
-    fish_add_path --prepend --move $actual_home/.local/bin
+    fish_add_path --prepend --move $actual_home/.local/share/uv/tools
     fish_add_path --prepend --move $actual_home/bin
+    fish_add_path --prepend --move $actual_home/.pixi/bin
+    fish_add_path --prepend --move $actual_home/.local/bin
+    fish_add_path --prepend --move $actual_home/.local/scripts
 else
     # Regular path setup for non-container environment
-    fish_add_path ~/.local/bin
+    fish_add_path ~/.local/share/uv/tools
     fish_add_path ~/bin
+    fish_add_path ~/.pixi/bin
+    fish_add_path ~/.local/bin
+    fish_add_path ~/.local/scripts
 end
 
--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -8,17 +8,3 @@ if status is-interactive
     end
 end
 
-# Check if we're in a distrobox container with a different $HOME
-if test -n "$CONTAINER_ID" -a -n "$DISTROBOX_HOST_HOME"
-    # Get the actual home directory path
-    set -l actual_home $HOME
-    
-    # Add container-specific paths with higher precedence
-    fish_add_path --prepend --move $actual_home/.pixi/bin
-    fish_add_path --prepend --move $actual_home/.local/scripts
-else
-    # Regular path setup for non-container environment
-    fish_add_path /home/markus/.pixi/bin
-    fish_add_path /home/markus/.local/scripts
-end
-
